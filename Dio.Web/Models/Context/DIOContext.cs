19c6cf3d4b9147888d5815c69b27148020a2a93a
Adriano Costa <adriano_amc@hotmail.com>
2021-06-19 20:18:11 -0300

    Add project files.

---
+++ Dio.Web/Models/Context/DIOContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dio.Web.Models.Context
+{
+    public class DIOContext: DbContext
+    {
+        public DbSet<Categoria> Categorias { get; set; }
+
+        public DbSet<Produto> Produtos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=DigitalDB;Integrated Security=True;");
+        }
+    }
+}
